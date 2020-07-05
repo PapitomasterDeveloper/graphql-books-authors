b79eb5abb866caa4ff6c976535dd6a95f863aaed
Miguel Angel Cavazos Reyes <miguel.cavazos@ad.infosys.com>
2020-07-05 13:21:38 -0500

    Created mutations for creating authors and books

--- server.js
+++ server.js
@@ -99,6 +99,40 @@ const RootQueryType = new GraphQLObjectType({
 	})
 })
 
+// Mutations are like POST, PUT, UPDATE, DELETE verbs in REST approach
+// The logic will be done if there is population, modification or removal of data
+const RootMutationType = new GraphQLObjectType({
+	name: 'Mutation',
+	description: 'Root Mutation',
+	fields: () => ({
+		addBook: {
+			type: BookType,
+			description: 'Add a book',
+			args: {
+				name: { type: GraphQLNonNull(GraphQLString) },
+				authorId: { type: GraphQLNonNull(GraphQLInt) }
+			},
+			resolve: (parent, args) => {
+				const book = { id: books.length + 1, name: args.name, authorId: args.authorId }
+				books.push(book)
+				return book
+			}
+		},
+		addAuthor: {
+                        type: AuthorType,
+                        description: 'Add a author',
+                        args: {
+                                name: { type: GraphQLNonNull(GraphQLString) }
+                        },
+                        resolve: (parent, args) => {
+                                const author = { id: authors.length + 1, name: args.name }
+                                authors.push(author)
+                                return author
+                        }
+                }
+	})
+});
+
 /*
 const schema = new GraphQLSchema({
         query: new GraphQLObjectType({
@@ -114,7 +148,8 @@ const schema = new GraphQLSchema({
 */
 
 const schema = new GraphQLSchema({
-	query: RootQueryType
+	query: RootQueryType,
+	mutation: RootMutationType
 });
 
 app.use('/graphql', expressGraphQL({
